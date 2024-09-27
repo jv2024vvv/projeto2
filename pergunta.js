dabc3aa40378287533708f5283e7bd5e5df7d3b7
jv2024vvv <161341357+jv2024vvv@users.noreply.github.com>
2024-09-27 11:14:41 -0300

    Create pergunta.js

---
+++ pergunta.js
@@ -0,0 +1,19 @@
+<!-- Trecho de código suprimido -->
+
+<script type="module" src="js/aleatorio.js"></script>
+<script type="module" src="js/perguntas.js"></script>
+<script type="module" src="js/script.js"></script>
+import {perguntas} from './perguntas.js';
+
+
+<div class="caixa-resultado">
+        <p class="texto-resultado"></p>
+        <buttton class="novamente-btn"></button>
+</div>
+
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
+
+<div class= “caixa-resultado”>
+    <p class=”texto-resultado”></p>
+    <button class=”novamente-btn”>Jogar novamente</button>
+</div>
